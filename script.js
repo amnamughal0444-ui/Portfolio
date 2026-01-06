a4f23af7cd664c05614cbd41e3dc851e6ce6a9b9
Amna Mughal <amnamughal0444@gmail.com>
2026-01-06 19:42:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+if (menu && navbar) {
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  };
+}
+
+window.onscroll = () => {
+  if (menu && navbar) {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
+};
+
+// ✅ Typed.js
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed(".multiple-text", {
+    strings: ["Front-End Developer", "Using HTML" , "CSS & JavaScript"],
+    typeSpeed: 50,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });
+});
